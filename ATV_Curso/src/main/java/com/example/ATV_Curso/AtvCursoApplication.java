a8f570ece336d11ff181c9d09db74773c661b47c
Davimazoti <davilmazoti07@gmail.com>
2025-02-26 14:09:14 -0300

    teste

---
+++ ATV_Curso/src/main/java/com/example/ATV_Curso/AtvCursoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ATV_Curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvCursoApplication.class, args);
+	}
+
+}
